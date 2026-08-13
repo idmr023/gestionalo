bd8be02b4d5e48343e205f47a65b6aed703abb16
Ivan Manrique <jparatupcya@gmail.com>
2026-08-13 13:33:29 -0500

    Solución

--- config/cache.php
+++ config/cache.php
@@ -3,8 +3,22 @@
 return [
     'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'file')),
     'stores' => [
-        'array' => ['driver' => 'array', 'serialize' => false],
-        'file' => ['driver' => 'file', 'path' => storage_path('framework/cache/data')],
+        'array' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
+        'database' => [
+            'driver' => 'database',
+            'connection' => env('DB_CACHE_CONNECTION'),
+            'table' => env('DB_CACHE_TABLE', 'cache'),
+            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
+            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+        ],
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
+        ],
     ],
     'prefix' => env('CACHE_PREFIX', 'gestionalo_cache'),
 ];
